ad2b3aef2ff02a6c222e6db78a3436587ef47835
Sean <C13455028@mydit.ie>
2015-09-28 15:59:41 +0000

    I made 4 version instead of 4 updates, fixing that

--- Lab3/Test3_Oxo.py
+++ Lab3/Test3_Oxo.py
@@ -1,9 +1,19 @@
-my_str1 = "0xo"
+# for loop used to compair all of them
+my_str[0] = "0xo"
+my_str[1] = "0XO"
+my_str[2] = "123454321"
+my_str3[3] = "ROTATOR"
+my_str5[4] = "12345 54321"
 
 
-my_str = my_strring.casefold()
-rev_str = reversed(my_strring)
-if list(my_string) == list(rev_str):
-   print("It is palindrome..closing")
-else:
-   print("It is not palindrome.. closeing")
+
+
+for (i=0;i<5;i++)
+{
+  my_str = my_strring.casefold()
+  rev_str = reversed(my_strring)
+  if list(my_str[i]) == list(rev_str):
+     print("It is palindrome..closing")
+  else:
+     print("It is not palindrome.. closeing")
+  }
