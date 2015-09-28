1c2d4cb65444741e669e541b0bc49647cf5abf20
Sean <C13455028@mydit.ie>
2015-09-28 15:39:27 +0000

    Adding the first option to see if file gets put up or not

---
+++ Lab3/Test3_Oxo.py
@@ -0,0 +1,9 @@
+my_str1 = "0xo"
+
+
+my_str = my_strring.casefold()
+rev_str = reversed(my_strring)
+if list(my_string) == list(rev_str):
+   print("It is palindrome..closing")
+else:
+   print("It is not palindrome.. closeing")
